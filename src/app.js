471628b3d9ffd9972203b11717853ce1a1fde4b7
鬼骨孖 <dxd.spirits@gmail.com>
2020-08-19 13:19:15 +0800

    debug app.onpageroute

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import store from './store/index'
 
 const App = new Vue({
   store,
-  onShow (options) {
+  onLaunch() {
+    try {
+      // 不要在 analytics 以外的场景使用 wx.onAppRoute, 这是个未公开的 API, 不能依赖于它
+      wx.onAppRoute(res => {
+        console.log('DEBUG: 检测到路由变化 (优化一下这部分的代码, 移动到 analytics 单独的包里)\n', res)
+      })
+    } catch(err) {
+      console.log(err)
+    }
+  },
+  onShow(options) {
   },
   render(h) {
     // this.$slots.default 是将要会渲染的页面
