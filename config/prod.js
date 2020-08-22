f914f3dc56e6a0e689f3b796343da8b4bad6aa11
鬼骨孖 <dxd.spirits@gmail.com>
2020-08-23 00:18:23 +0800

    生产环境编译sass不要忽略注释

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,14 @@ module.exports = {
   defineConstants: {
     API_URL: '"https://heidianapi.com/api/"'
   },
-  mini: {},
+  mini: {
+    sassLoaderOption: {
+      sassOptions: {
+        // 这么配置让生产环境编译 sass 前不要去除注释, 不然 /*postcss-pxtransform disable*/ 会失效
+        outputStyle: 'expanded'
+      }
+    }
+  },
   h5: {
     /**
      * 如果h5端编译后体积过大，可以使用webpack-bundle-analyzer插件对打包体积进行分析。
