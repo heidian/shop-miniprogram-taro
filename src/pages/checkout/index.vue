f3f6c0ccdbad280efdf388b96df9a5568c112a8f
鬼骨孖 <dxd.spirits@gmail.com>
2020-08-30 11:35:02 +0800

    move checkout styles to .scss file

--- src/pages/checkout/index.vue
+++ src/pages/checkout/index.vue
@@ -25,6 +25,7 @@ import { getCurrentInstance } from '@tarojs/taro'
 // import { API } from '@/utils/api'
 import { optimizeImage, backgroundImageUrl } from '@/utils/image'
 import { mapState } from 'vuex'
+import './index.scss'
 
 export default {
   name: 'Checkout',
@@ -53,82 +54,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-$color-primary: #ff5a00;
-$color-accent: #ff5a00;
-$color-divider: #f8f8f8;
-.page--checkout {
-  min-height: 100vh;
-  background-color: #f8f8f8;
-  overflow: hidden;
-  /* section */
-  > .section {
-    border-radius: 6px;
-    background-color: #fff;
-    margin: 8px;
-    padding: 12px;
-  }
-  .section__header {
-    margin-bottom: 12px;
-    // font-size: 0.95em;
-    font-weight: bold;
-  }
-  .price-line {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    & + .price-line {
-      margin-top: 10px;
-    }
-  }
-  /* footer */
-  > .footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    background-color: #fff;
-    padding: 10px 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .button--payfororder {
-    //
-  }
-}
-/* 一些通用样式, 暂时先不移动到全局 app.scss 里面, 目前很多基础样式还没定
-页面之间的样式在小程序里不会冲突, 只要同一个页面的组件之间区分好 class 就行 */
-button {
-  // button 标签是小程序和网页通用的, 这里定义样式的时候可以不用 css
-  display: inline-block;
-  font-size: 13px;
-  line-height: 20px;
-  padding: 8px 12px;
-  // button 高度是 36
-  border: 0;
-  margin: 0;
-  &::after {
-    display: none;
-  }
-}
-button.button--round {
-  border-radius: 18px;
-  padding-left: 16px;
-  padding-right: 16px;
-}
-button.button--primary {
-  color: #fff;
-  background-color: $color-primary;
-  &.button-hover {
-    color: rgba(#fff, 0.9);
-    background-color: lighten($color-primary, 5%);
-  }
-}
-.highlight {
-  color: $color-accent;
-}
-.bold {
-  font-weight: bold;
-}
-</style>
+<style lang="scss"></style>
