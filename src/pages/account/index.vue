e376d53a572f8b79453ef7b027b639bb4016a765
liran319 <liran319@gmail.com>
2020-09-02 17:31:54 +0800

    add infinite products in account page

--- src/pages/account/index.vue
+++ src/pages/account/index.vue
@@ -1,127 +1,139 @@
 <template>
   <view class="page page--account">
     <!-- 普通的跳转页面, 只需要 url 就行了 -->
-    <view class="account__customer">
-      <view class="account__customer__main">
-        <image
-          class="account__customer__avatar"
-          mode="aspectFill"
-          :src="customer.avatar || 'https://up.img.heidiancdn.com/o_1cm7ccaoirfi1tdiutsn6s1odj0rofile.png?imageView2/2/w/360/ignore-error/1'"></image>
-        <view class="account__customer__caption" v-if="isAuthenticated">
-          <view class="account__customer__caption__name">
-            <text class="account__customer__full-name">{{ customer.full_name || '未命名' }}</text>
-            <text class="account__customer__level__title">{{ levelTitle }}</text>
-            <navigator class="account__customer__navigator--text" url="/pages/profile/index" open-type="navigate" hover-class="none">填写微信号</navigator>
+    <view class="account__dark">
+      <view class="account__customer">
+        <view class="account__customer__main">
+          <image
+            class="account__customer__avatar"
+            mode="aspectFill"
+            :src="customer.avatar || 'https://up.img.heidiancdn.com/o_1cm7ccaoirfi1tdiutsn6s1odj0rofile.png?imageView2/2/w/360/ignore-error/1'"></image>
+          <view class="account__customer__caption" v-if="isAuthenticated">
+            <view class="account__customer__caption__name">
+              <text class="account__customer__full-name">{{ customer.full_name || '未命名' }}</text>
+              <text class="account__customer__level__title">{{ levelTitle }}</text>
+              <navigator class="account__customer__navigator--text" url="/pages/profile/index" open-type="navigate" hover-class="none">填写微信号</navigator>
+            </view>
+            <view class="account__customer__caption__referral">
+              <text class="account__customer__referral-code">邀请ID：{{ customer.referral_code }}</text>
+              <button class="account__customer__btn--copy-referral-code" @tap="onCopyToClipboard(customer.referral_code)">复制</button>
+            </view>
           </view>
-          <view class="account__customer__caption__referral">
-            <text class="account__customer__referral-code">邀请ID：{{ customer.referral_code }}</text>
-            <button class="account__customer__btn--copy-referral-code" @tap="onCopyToClipboard(customer.referral_code)">复制</button>
+          <view v-else class="account__customer__caption">
+            <navigator url="/pages/login/index" class="account__customer__login">登录</navigator>
+          </view>
+          <navigator class="account__customer__caret" url="/pages/profile/index" open-type="navigate" hover-class="none"></navigator>
+        </view>
+        <view class="account__customer__level-progress">
+          <view class="account__customer__level__hint">
+            <view class="account__customer__level__hint__text">当前成长值{{ points }}（达{{ pointsMax }}即可升级）</view>
+            <view class="account__customer__level__hint__number">{{ points }}/{{ pointsMax }} <view class="account__text__caret"></view></view>
+          </view>
+          <view class="account__customer__level__progress">
+            <view class="account__customer__level__progress__inner" :style="levelProgressStyle"></view>
           </view>
         </view>
-        <view v-else class="account__customer__caption">
-          <navigator url="/pages/login/index" class="account__customer__login">登录</navigator>
-        </view>
-        <navigator class="account__customer__caret" url="/pages/profile/index" open-type="navigate" hover-class="none"></navigator>
       </view>
-      <view class="account__customer__level-progress">
-        <view class="account__customer__level__hint">
-          <view class="account__customer__level__hint__text">当前成长值{{ points }}（达{{ pointsMax }}即可升级）</view>
-          <view class="account__customer__level__hint__number">{{ points }}/{{ pointsMax }} <view class="account__text__caret"></view></view>
+
+      <template>
+
+        <view class="account__partner">
+          <text class="account__partner__text">成为合伙人获取收益</text>
+          <text class="account__partner__btn__text">立即升级</text>
         </view>
-        <view class="account__customer__level__progress">
-          <view class="account__customer__level__progress__inner" :style="levelProgressStyle"></view>
+
+        <view class="account__balance">
+          <view class="account__balance__main">
+            <view class="account__balance__values">
+              <text class="account__balance__values__label">账户余额（元）</text>
+              <text class="account__balance__values__number">{{ balance }}</text>
+              <text class="account__balance__values__hint">自购返利￥0+邀请收益￥0</text>
+            </view>
+            <view class="account__balance__btns">
+              <view class="account__balance__btns__withdraw-history">提现记录 <view class="account__text__caret"></view></view>
+              <navigator class="account__balance__btns__bind-alipay" url="/pages/bind-alipay/index" open-type="navigate" hover-class="none">绑定支付宝</navigator>
+            </view>
+          </view>
+          <view class="account__balance__divider"></view>
+          <view class="account__balance__summary">
+            <view class="account__balance__summary__item">
+              <view class="account__balance__summary__item__label">今日预估奖励</view>
+              <view class="account__balance__summary__item__value">0</view>
+            </view>
+            <view class="account__balance__summary__item">
+              <view class="account__balance__summary__item__label">本月预估奖励</view>
+              <view class="account__balance__summary__item__value">0</view>
+            </view>
+            <view class="account__balance__summary__item">
+              <view class="account__balance__summary__item__label">已邀请粉丝</view>
+              <view class="account__balance__summary__item__value">0</view>
+            </view>
+          </view>
         </view>
-      </view>
+
+        <view class="account__grid account__promotion">
+          <navigator
+            v-for="item in promotionNavigators" :key="item.text"
+            class="account__grid__item account__grid__navigator"
+            open-type="navigate"
+            hover-class="none">
+            <image class="account__grid__item__icon account__grid__item__icon--bigger" :src="item.icon" mode="aspectFit"></image>
+            <text class="account__grid__item__text">{{ item.text }}</text>
+          </navigator>
+        </view>
+
+        <view class="account__section">
+          <view class="account__section__head">
+            <text class="account__section__head__title">我的订单</text>
+            <navigator class="account__section__head__navigator" open-type="navigate" hover-class="none">
+              <text>查看全部</text><view class="account__text__caret"></view>
+            </navigator>
+          </view>
+          <view class="account__grid">
+            <navigator
+              v-for="item in orderNavigators" :key="item.text"
+              class="account__grid__item account__grid__navigator account__grid__navigator--divider"
+              open-type="navigate"
+              hover-class="none">
+              <image class="account__grid__item__icon" :src="item.icon" mode="aspectFit"></image>
+              <text class="account__grid__item__text">{{ item.text }}</text>
+            </navigator>
+          </view>
+        </view>
+
+        <view class="account__section">
+          <view class="account__section__head">
+            <text class="account__section__head__title">我的功能</text>
+          </view>
+          <view class="account__grid">
+            <navigator
+              v-for="item in otherNavigators" :key="item.text"
+              class="account__grid__item account__grid__navigator account__grid__navigator--divider"
+              open-type="navigate"
+              hover-class="none">
+              <image class="account__grid__item__icon" :src="item.icon" mode="aspectFit"></image>
+              <text class="account__grid__item__text">{{ item.text }}</text>
+            </navigator>
+          </view>
+        </view>
+      </template>
     </view>
-
-    <template>
-
-      <view class="account__partner">
-        <text class="account__partner__text">成为合伙人获取收益</text>
-        <text class="account__partner__btn__text">立即升级</text>
-      </view>
-
-      <view class="account__balance">
-        <view class="account__balance__main">
-          <view class="account__balance__values">
-            <text class="account__balance__values__label">账户余额（元）</text>
-            <text class="account__balance__values__number">{{ balance }}</text>
-            <text class="account__balance__values__hint">自购返利￥0+邀请收益￥0</text>
-          </view>
-          <view class="account__balance__btns">
-            <view class="account__balance__btns__withdraw-history">提现记录 <view class="account__text__caret"></view></view>
-            <navigator class="account__balance__btns__bind-alipay" url="/pages/bind-alipay/index" open-type="navigate" hover-class="none">绑定支付宝</navigator>
-          </view>
-        </view>
-        <view class="account__balance__divider"></view>
-        <view class="account__balance__summary">
-          <view class="account__balance__summary__item">
-            <view class="account__balance__summary__item__label">今日预估奖励</view>
-            <view class="account__balance__summary__item__value">0</view>
-          </view>
-          <view class="account__balance__summary__item">
-            <view class="account__balance__summary__item__label">本月预估奖励</view>
-            <view class="account__balance__summary__item__value">0</view>
-          </view>
-          <view class="account__balance__summary__item">
-            <view class="account__balance__summary__item__label">已邀请粉丝</view>
-            <view class="account__balance__summary__item__value">0</view>
-          </view>
-        </view>
-      </view>
-
-      <view class="account__grid account__promotion">
-        <navigator
-          v-for="item in promotionNavigators" :key="item.text"
-          class="account__grid__item account__grid__navigator"
-          open-type="navigate"
-          hover-class="none">
-          <image class="account__grid__item__icon account__grid__item__icon--bigger" :src="item.icon" mode="aspectFit"></image>
-          <text class="account__grid__item__text">{{ item.text }}</text>
-        </navigator>
-      </view>
-
-      <view class="account__section">
-        <view class="account__section__head">
-          <text class="account__section__head__title">我的订单</text>
-          <navigator class="account__section__head__navigator" open-type="navigate" hover-class="none">
-            <text>查看全部</text><view class="account__text__caret"></view>
-          </navigator>
-        </view>
-        <view class="account__grid">
-          <navigator
-            v-for="item in orderNavigators" :key="item.text"
-            class="account__grid__item account__grid__navigator account__grid__navigator--divider"
-            open-type="navigate"
-            hover-class="none">
-            <image class="account__grid__item__icon" :src="item.icon" mode="aspectFit"></image>
-            <text class="account__grid__item__text">{{ item.text }}</text>
-          </navigator>
-        </view>
-      </view>
-
-      <view class="account__section">
-        <view class="account__section__head">
-          <text class="account__section__head__title">我的功能</text>
-        </view>
-        <view class="account__grid">
-          <navigator
-            v-for="item in otherNavigators" :key="item.text"
-            class="account__grid__item account__grid__navigator account__grid__navigator--divider"
-            open-type="navigate"
-            hover-class="none">
-            <image class="account__grid__item__icon" :src="item.icon" mode="aspectFit"></image>
-            <text class="account__grid__item__text">{{ item.text }}</text>
-          </navigator>
-        </view>
-      </view>
-    </template>
 
     <!-- <view class="account__auth-btns">
       <button v-if="isAuthenticated"  class="account__auth__btn account__auth__btn--red" @tap="logout">登出</button>
     </view> -->
-
-    <!-- <infinite-products ref="infiniteProducts"></infinite-products> -->
+    <view class="account__light account__infinite-products">
+      <view class="account__infinite-products__head">
+        <image class="account__infinite-products__logo" mode="aspectFill" :src="'https://up.img.heidiancdn.com/o_1eh71dvj035vlmd1251d2b14on0up412x.png'|imageUrl(300)"></image>
+        <navigator class="account__infinite-products__navigator">
+          <text class="account__infinite-products__navigator__text">查看全部</text>
+          <image class="account__infinite-products__navigator__arrow" mode="aspectFit" src="https://up.img.heidiancdn.com/o_1eh71g4odmlspfo1ubjqc196a0copy2x.png"></image>
+        </navigator>
+      </view>
+      <view class="account__infinite-products__body">
+        <infinite-products ref="infiniteProducts"></infinite-products>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -131,7 +143,7 @@ import { mapState } from 'vuex'
 import _ from 'lodash'
 import { handleErr } from '@/utils/errHelper'
 
-// import InfiniteProducts from '@/components/InfiniteProducts/InfiniteProducts'
+import InfiniteProducts from '@/components/InfiniteProducts/InfiniteProducts'
 
 import './index.scss'
 
@@ -209,7 +221,7 @@ export default {
     }
   },
   components: {
-    // InfiniteProducts,
+    InfiniteProducts,
   },
   computed: {
     ...mapState('customer', {
