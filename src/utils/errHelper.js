583b230233d3c300fa559a4feb9e4285f4b84845
鬼骨孖 <dxd.spirits@gmail.com>
2020-09-04 23:24:59 +0800

    improve errHelper

--- src/utils/errHelper.js
+++ src/utils/errHelper.js
@@ -1,10 +1,15 @@
-const handleErr = (err) => {
-  Taro.hideLoading()
+import _ from 'lodash'
+import Taro from '@tarojs/taro'
+
+export const handleErr = (err) => {
+  // Taro.hideLoading()  // 不应该写这个
   if (err.response && (err.response.status == 403 || err.response.status == 401)) {
-    Taro.showToast({
-      icon: 'none',
-      title: '请登录后继续操作'
-    })
+    // 403 和 401 不要做任何操作, 有时候这种错误未必就是登录就能解决的
+    // 界面上应该自己按需处理, 这里可以再 export 一个新的方法 "requireLogin" 来专门处理登录跳转
+    // Taro.showToast({
+    //   icon: 'none',
+    //   title: '请登录后继续操作'
+    // })
   } else if (err.response && err.response.data) {
     var errors = []
     for (var key in err.response.data) {
@@ -18,24 +23,20 @@ const handleErr = (err) => {
       success: function(res) {}
     })
   } else {
-    let err_msg = ''
-    if (err && err.detail) {
-      err_msg = err.detail
-    } else if (err.errMsg) {
-      err_msg = err.errMsg
-    } else if (typeof err === 'object') {
-      err_msg = JSON.stringify(err)
+    console.log(err)  // 遇到未知错误, 都打印出来, 好后期检查错误栈
+    let errMsg = ''
+    if (err instanceof Error) {
+      errMsg = err.detail || err.errMsg || ('' + err)
+    } else if (_.isPlainObject(err) || _.isArray(err)) {
+      errMsg = JSON.stringify(err)
     } else {
-      err_msg = err.toString()
+      errMsg = '' + err
     }
     Taro.showModal({
       title: '出错了',
-      content: err_msg,
+      content: errMsg,
       showCancel: false,
       success: function(res) {}
     })
   }
-}
-export {
-  handleErr
 }
