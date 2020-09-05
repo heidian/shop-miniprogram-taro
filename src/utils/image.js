bd8594842eeccb7ffef4c5879f80901fd1d0bf17
鬼骨孖 <dxd.spirits@gmail.com>
2020-09-06 01:37:17 +0800

    convert big png image to jpg

--- src/utils/image.js
+++ src/utils/image.js
@@ -2,7 +2,9 @@ import qs from 'qs'
 import _ from 'lodash'
 
 export const optimizeImage = (url, width, height) => {
+  let metafield = {}
   if (typeof url === 'object') {
+    metafield = _.get(url, 'metafield') || ''
     url = _.get(url, 'src') || ''
   }
   if (!url || !_.isString(url)) {
@@ -12,7 +14,7 @@ export const optimizeImage = (url, width, height) => {
     return url
   }
   if (!width) {
-    width = 600
+    width = 400
   }
   // const ratio = Math.ceil(global.devicePixelRatio || 1)
   // 小程序的 devicePixelRatio 不是这么取的, 先固定成 2
@@ -33,6 +35,13 @@ export const optimizeImage = (url, width, height) => {
     optimQueries.push(`imageView2/2/w/${width * ratio}/h/${height * ratio}/ignore-error/1`)
   } else {
     optimQueries.push(`imageView2/2/w/${width * ratio}/ignore-error/1`)
+  }
+  // if (/\.png$/.test(url) ||
+  if (metafield.mimeType === 'image/png' && (+metafield.size > 500 * 1024)) {
+    // 小程序现在支持 webp 了, 强制转换, 任何格式都可以转换成 webp
+    // 这个还不大确定兼容性, 目前暂时还是转成 jpg, 并且只对大的 png 图片进行处理
+    // optimQueries.push(`imageView2/2/format/webp/ignore-error/1`)
+    optimQueries.push(`imageView2/2/format/jpg/ignore-error/1`)
   }
   return optimUrl + optimQueries.join('|')
 }
