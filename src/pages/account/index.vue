21484acc241eb396ba19a128d52b98643b9bedfc
鬼骨孖 <dxd.spirits@gmail.com>
2020-09-15 01:14:37 +0800

    remove uesless account class

--- src/pages/account/index.vue
+++ src/pages/account/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <view :class="$style['account']">
+  <view :class="$style['page']">
     <!-- 普通的跳转页面, 只需要 url 就行了 -->
     <view :class="$style['darkMode']">
       <view :class="$style['customer']">
@@ -267,341 +267,341 @@ $color-brown: #5c411a;
 $color-box-shadow: rgba(0, 0, 0, 0.2);
 $color-divider: rgba(#ffffff, 0.1);
 
-.account {
-  .darkMode {
-    background-color: $color-bg;
-    color: $color-text;
-    padding: 25px 20px 50px;
-  }
-  .main {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-  }
-  .login {
-    font-size: 16px;
-  }
-  .avatar {
-    width: 50px;
-    height: 50px;
-    border-radius: 25px;
-    overflow: hidden;
-  }
-  .caption {
-    flex: 1;
-    padding: 0 0 0 15px;
-    font-size: 12px;
-  }
-  .captionName {
-    width: 100%;
-    display: flex;
-    justify-content: flex-start;
-    align-items: baseline;
-  }
-  .referral {
-    margin-top: 5px;
-    width: 100%;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  }
-  .fullName {
-    font-size: 15px;
-  }
-  .levelTitle {
-    margin-left: 10px;
-    color: $color-text-light;
-  }
-  .navigatorText {
-    margin-left: 10px;
-    color: $color-golden;
-  }
-  .referralCode {
-    line-height: 1;
-    padding: 4px 6px;
-    background-color: $color-bg-dark;
-    border-radius: 2px;
-  }
-  .btnCopy {
-    background-color: transparent;
-    font-size: 12px !important;
-    padding: 3px 6px;
-    margin: 0;
-    margin-left: 10px;
-    line-height: 1;
-    color: $color-golden;
-    border: 1px solid $color-golden;
-    border-radius: 2px;
-  }
-  .caret {
-    margin-right: -15px;
-    width: 40px;
-    height: 40px;
-    position: relative;
-  }
-  .caret::after {
-    content: '';
-    position: absolute;
-    width: 10px;
-    height: 10px;
-    border-top: 2px solid $color-text;
-    border-right: 2px solid $color-text;
-    top: 50%;
-    left: 50%;
-    margin-left: -5px;
-    margin-top: -5px;
-    transform: rotate(45deg);
-  }
-
-  .level {
-    width: 100%;
-    margin-top: 15px;
-  }
-  .levelHint {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    font-size: 12px;
-    color: $color-text-light;
-  }
-  .levelHintNumber {
-    display: flex;
-    align-items: center;
-    color: $color-text;
-    font-size: 11px;
-  }
-  .textCaret {
-    display: inline-block;
-    margin-left: 3px;
-    width: 6px;
-    height: 6px;
-    border-top: 1px solid $color-text-light;
-    border-right: 1px solid $color-text-light;
-    transform: rotate(45deg);
-  }
-  .levelProgress {
-    width: 100%;
-    margin-top: 8px;
-    overflow: hidden;
-    height: 8px;
-    border-radius: 4px;
-    background-color: $color-bg-progress;
-  }
-  .levelProgressInner {
-    display: block;
-    width: 0;
-    height: 100%;
-    background-color: $color-golden;
-  }
-
-  /* partner */
-  .partner {
-    width: 100%;
-    height: 34px;
-    margin-top: 15px;
-    border-radius: 17px;
-    background: linear-gradient(135deg, #020202 66.7%, $color-golden 33.3%);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .partnerText {
-    font-size: 15px;
-    width: 64%;
-    text-align: center;
-    color: $color-golden;
-    font-weight: normal;
-  }
-  .partnerBtnText {
-    width: 32%;
-    font-size: 14px;
-    font-weight: bold;
-    text-align: center;
-    color: $color-brown;
-  }
-
-  /* balance */
-  .balance {
-    margin-top: 15px;
-    width: 100%;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 2px 4px 10px 0 $color-box-shadow;
-    background-image: linear-gradient(126deg, $color-bg-light, $color-bg 87%);
-    color: $color-text-light;
-  }
-  .balanceMain {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .balanceValues {
-    display: flex;
-    flex-direction: column;
-    color: $color-text;
-  }
-  .balanceValuesLabel {
-    font-size: 13px;
-  }
-  .balanceValuesNumber {
-    font-size: 22px;
-  }
-  .balanceValuesHint {
-    font-size: 10px;
-  }
-  .balanceBtns {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-end;
-  }
-  .withdrawHistory {
-    color: $color-text;
-    font-size: 12px;
-    display: flex;
-    align-items: center;
-  }
-  .bindAlipay {
-    margin-top: 20px;
-    font-size: 14px;
-    border-radius: 4px;
-    background-color: $color-golden;
-    color: $color-brown;
-    width: 100px;
-    height: 30px;
-    line-height: 30px;
-    padding: 0;
-    text-align: center;
-  }
-
-  .balanceDivider {
-    width: 100%;
-    height: 0;
-    border-top: 1px solid $color-divider;
-    margin: 15px 0;
-  }
-
-  .balanceSummary {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .balanceSummaryItem {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .balanceSummaryLabel {
-    font-size: 12px;
-    line-height: 1;
-    margin-bottom: 10px;
-  }
-  .balanceSummaryValue {
-    font-size: 18px;
-    color: $color-text;
-    line-height: 1;
-  }
-
-  .grid {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .gridItem {
-    flex: 1;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .gridItemIcon {
-    width: 20px;
-    height: 20px;
-    margin-bottom: 15px;
-  }
-  .gridItemIconBigger {
-    width: 25px;
-    height: 25px;
-  }
-  .gridItemText {
-    font-size: 13px;
-    line-height: 1;
-    color: $color-text-light;
-  }
-
-  .promotion {
-    margin-top: 30px;
-  }
-
-  .section {
-    margin-top: 30px;
-  }
-  .sectionHead {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-bottom: 7px;
-    border-bottom: 1px solid $color-divider;
-    margin-bottom: 10px;
-  }
-  .sectionHeadTitle {
-    font-size: 15px;
-    font-weight: 600;
-    color: $color-text;
-  }
-  .sectionHeadNavigator {
-    display: flex;
-    align-items: center;
-    font-size: 12px;
-    color: $color-text-light;
-  }
-  .gridNavigatorDivider:not(:last-child)::after {
-    content: '';
-    position: absolute;
-    top: 10px;
-    bottom: 10px;
-    right: 0;
-    border-right: 1px solid $color-divider;
-  }
-
-  .light {
-    padding: 20px 0;
-    background-color: $color-bg-lighter;
-  }
-  .infiniteProductsHead {
-    width: 100%;
-    padding: 15px;
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
-  }
-  .infiniteProductsLogo {
-    width: 216px;
-    height: 72px;
-  }
-  .infiniteProductsNavigator {
-    display: flex;
-    align-items: center;
-  }
-  .infiniteProductsNavigatorText {
-    color: $color-text-dark;
-    font-size: 12px;
-    line-height: 22px;
-    border-bottom: 1px solid;
-  }
-  .infiniteProductsNavigatorArrow {
-    margin-left: 4px;
-    width: 22px;
-    height: 6px;
-  }
+.page {
+  //
+}
+.darkMode {
+  background-color: $color-bg;
+  color: $color-text;
+  padding: 25px 20px 50px;
+}
+.main {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+}
+.login {
+  font-size: 16px;
+}
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  overflow: hidden;
+}
+.caption {
+  flex: 1;
+  padding: 0 0 0 15px;
+  font-size: 12px;
+}
+.captionName {
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: baseline;
+}
+.referral {
+  margin-top: 5px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.fullName {
+  font-size: 15px;
+}
+.levelTitle {
+  margin-left: 10px;
+  color: $color-text-light;
+}
+.navigatorText {
+  margin-left: 10px;
+  color: $color-golden;
+}
+.referralCode {
+  line-height: 1;
+  padding: 4px 6px;
+  background-color: $color-bg-dark;
+  border-radius: 2px;
+}
+.btnCopy {
+  background-color: transparent;
+  font-size: 12px !important;
+  padding: 3px 6px;
+  margin: 0;
+  margin-left: 10px;
+  line-height: 1;
+  color: $color-golden;
+  border: 1px solid $color-golden;
+  border-radius: 2px;
+}
+.caret {
+  margin-right: -15px;
+  width: 40px;
+  height: 40px;
+  position: relative;
+}
+.caret::after {
+  content: '';
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  border-top: 2px solid $color-text;
+  border-right: 2px solid $color-text;
+  top: 50%;
+  left: 50%;
+  margin-left: -5px;
+  margin-top: -5px;
+  transform: rotate(45deg);
 }
 
+.level {
+  width: 100%;
+  margin-top: 15px;
+}
+.levelHint {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+  color: $color-text-light;
+}
+.levelHintNumber {
+  display: flex;
+  align-items: center;
+  color: $color-text;
+  font-size: 11px;
+}
+.textCaret {
+  display: inline-block;
+  margin-left: 3px;
+  width: 6px;
+  height: 6px;
+  border-top: 1px solid $color-text-light;
+  border-right: 1px solid $color-text-light;
+  transform: rotate(45deg);
+}
+.levelProgress {
+  width: 100%;
+  margin-top: 8px;
+  overflow: hidden;
+  height: 8px;
+  border-radius: 4px;
+  background-color: $color-bg-progress;
+}
+.levelProgressInner {
+  display: block;
+  width: 0;
+  height: 100%;
+  background-color: $color-golden;
+}
+
+/* partner */
+.partner {
+  width: 100%;
+  height: 34px;
+  margin-top: 15px;
+  border-radius: 17px;
+  background: linear-gradient(135deg, #020202 66.7%, $color-golden 33.3%);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.partnerText {
+  font-size: 15px;
+  width: 64%;
+  text-align: center;
+  color: $color-golden;
+  font-weight: normal;
+}
+.partnerBtnText {
+  width: 32%;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  color: $color-brown;
+}
+
+/* balance */
+.balance {
+  margin-top: 15px;
+  width: 100%;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 2px 4px 10px 0 $color-box-shadow;
+  background-image: linear-gradient(126deg, $color-bg-light, $color-bg 87%);
+  color: $color-text-light;
+}
+.balanceMain {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.balanceValues {
+  display: flex;
+  flex-direction: column;
+  color: $color-text;
+}
+.balanceValuesLabel {
+  font-size: 13px;
+}
+.balanceValuesNumber {
+  font-size: 22px;
+}
+.balanceValuesHint {
+  font-size: 10px;
+}
+.balanceBtns {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+}
+.withdrawHistory {
+  color: $color-text;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+}
+.bindAlipay {
+  margin-top: 20px;
+  font-size: 14px;
+  border-radius: 4px;
+  background-color: $color-golden;
+  color: $color-brown;
+  width: 100px;
+  height: 30px;
+  line-height: 30px;
+  padding: 0;
+  text-align: center;
+}
+
+.balanceDivider {
+  width: 100%;
+  height: 0;
+  border-top: 1px solid $color-divider;
+  margin: 15px 0;
+}
+
+.balanceSummary {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.balanceSummaryItem {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.balanceSummaryLabel {
+  font-size: 12px;
+  line-height: 1;
+  margin-bottom: 10px;
+}
+.balanceSummaryValue {
+  font-size: 18px;
+  color: $color-text;
+  line-height: 1;
+}
+
+.grid {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.gridItem {
+  flex: 1;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.gridItemIcon {
+  width: 20px;
+  height: 20px;
+  margin-bottom: 15px;
+}
+.gridItemIconBigger {
+  width: 25px;
+  height: 25px;
+}
+.gridItemText {
+  font-size: 13px;
+  line-height: 1;
+  color: $color-text-light;
+}
+
+.promotion {
+  margin-top: 30px;
+}
+
+.section {
+  margin-top: 30px;
+}
+.sectionHead {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 7px;
+  border-bottom: 1px solid $color-divider;
+  margin-bottom: 10px;
+}
+.sectionHeadTitle {
+  font-size: 15px;
+  font-weight: 600;
+  color: $color-text;
+}
+.sectionHeadNavigator {
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  color: $color-text-light;
+}
+.gridNavigatorDivider:not(:last-child)::after {
+  content: '';
+  position: absolute;
+  top: 10px;
+  bottom: 10px;
+  right: 0;
+  border-right: 1px solid $color-divider;
+}
+
+.light {
+  padding: 20px 0;
+  background-color: $color-bg-lighter;
+}
+.infiniteProductsHead {
+  width: 100%;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+}
+.infiniteProductsLogo {
+  width: 216px;
+  height: 72px;
+}
+.infiniteProductsNavigator {
+  display: flex;
+  align-items: center;
+}
+.infiniteProductsNavigatorText {
+  color: $color-text-dark;
+  font-size: 12px;
+  line-height: 22px;
+  border-bottom: 1px solid;
+}
+.infiniteProductsNavigatorArrow {
+  margin-left: 4px;
+  width: 22px;
+  height: 6px;
+}
 </style>
