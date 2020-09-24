9e025974ae3404b6e7e6393173ea1faf10bd681e
鬼骨孖 <dxd.spirits@gmail.com>
2020-09-24 23:25:14 +0800

    style checkout

--- src/pages/checkout/index.vue
+++ src/pages/checkout/index.vue
@@ -1,59 +1,60 @@
 <template>
-  <view class="page--checkout">
+  <view :class="$style['page']">
     <!-- <view class="section">drawerVisible: {{ couponCodesDrawerVisible }}</view> -->
-    <view class="section shipping-address-summary" @tap="goToAddress">
+    <view :class="[$style['section'], $style['shippingAddressSummary']]" @tap="goToAddress">
       <view v-if="getField('shipping_address')">
-        <view class="area">{{ getField('shipping_address.province') }} {{ getField('shipping_address.city') }} {{ getField('shipping_address.district') }}</view>
-        <view class="address">{{ getField('shipping_address.address1') }}{{ getField('shipping_address.address2') }}</view>
-        <view class="contact">{{ getField('shipping_address.full_name') }} {{ getField('shipping_address.mobile') }}</view>
+        <view :class="$style['area']">{{ getField('shipping_address.province') }} {{ getField('shipping_address.city') }} {{ getField('shipping_address.district') }}</view>
+        <view :class="$style['address']">{{ getField('shipping_address.address1') }}{{ getField('shipping_address.address2') }}</view>
+        <view :class="$style['contact']">{{ getField('shipping_address.full_name') }} {{ getField('shipping_address.mobile') }}</view>
       </view>
-      <view class="caret-right" style="margin-left: auto;"></view>
+      <view v-else><text class="el-icon-circle-plus-outline"></text>添加收货地址</view>
+      <view :class="$style['caret']" style="margin-left: auto;"><text class="el-icon-arrow-right"></text></view>
     </view>
-    <view class="section">
+    <view :class="$style['section']">
       <!-- <view class="section__header">商品</view> -->
-      <view class="lines-summary" @tap="showLinesDrawer">
+      <view :class="$style['linesSummary']" @tap="showLinesDrawer">
         <image
           v-for="(line, index) in (getField('lines') || [])" :key="line.id"
           v-if="index < 3" :src="optimizeImage(line.image, 100)"
-          class="line-image" mode="aspectFill"
+          :class="$style['lineImage']" mode="aspectFill"
         ></image>
         <view style="margin-left: auto; margin-right: 0.5em; text-align: center;">
           <view>共 {{ (getField('lines') || []).length }} 件</view>
-          <view class="text-tip">(可选配送方式)</view>
+          <!-- <view :class="$style['textTip']">(可选配送方式)</view> -->
         </view>
-        <view class="caret-right"></view>
+        <view :class="$style['caret']"><text class="el-icon-arrow-right"></text></view>
       </view>
-      <view class="coupon-codes-summary" @tap="showCouponCodesDrawer">
+      <view :class="$style['couponCodesSummary']" @tap="showCouponCodesDrawer">
         <view>优惠券</view>
-        <view style="margin-left: auto; margin-right: 0.5em;" class="text-tip">
+        <view style="margin-left: auto; margin-right: 0.5em;" :class="$style['textTip']">
           <template v-if="getField('coupon_codes[0]')">已选择优惠券: {{ getField('coupon_codes[0].title') }}</template>
           <template v-else>选择优惠券</template>
         </view>
-        <view class="caret-right"></view>
+        <view :class="$style['caret']"><text class="el-icon-arrow-right"></text></view>
       </view>
     </view>
     <!-- <view class="section">支付方式, 因为没法选择, 这个不显示</view> -->
-    <view class="section">
-      <view class="price-line">
+    <view :class="$style['section']">
+      <view :class="$style['priceLine']">
         <text>商品金额</text>
         <price :price="getField('total_line_items_price')" :keepZero="true"></price>
       </view>
-      <view class="price-line">
+      <view :class="$style['priceLine']">
         <text>运费</text>
         <price :price="getField('shipping_cost')" :keepZero="true"></price>
       </view>
-      <view class="price-line" v-for="discount in getField('discounts')" :key="discount.id">
+      <view :class="$style['priceLine']" v-for="discount in getField('discounts')" :key="discount.id">
         <text>{{ discount.description }}</text>
         <price :price="-discount.discount_price" :keepZero="true"></price>
       </view>
     </view>
-    <view class="footer">
+    <view :class="$style['footer']">
       <view>
         <text>实付金额:</text>
         <price :price="finalPrice" :highlight="true" :keepZero="true"></price>
       </view>
       <button
-        class="button--payfororder button--round button--orange"
+        :class="[$style['buttonPayForOrder'], 'button--round', 'button--orange']"
         :disabled="paymentPending" @tap="pay"
       >{{ paymentPending ? '正在支付...' : '立即支付' }}</button>
     </view>
@@ -69,7 +70,6 @@ import { API } from '@/utils/api'
 import { optimizeImage, backgroundImageUrl } from '@/utils/image'
 import Price from '@/components/Price'
 import AvailableCouponCodes from './AvailableCouponCodes'
-import './index.scss'
 
 export default {
   name: 'Checkout',
@@ -177,4 +177,94 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" module>
+@import '@/styles/variables';
+page {
+  background-color: $color-bg-gray;
+}
+.page {
+  overflow: hidden;
+  padding-bottom: 80px;
+}
+/* section */
+.section {
+  border-radius: 6px;
+  background-color: #fff;
+  margin: 8px;
+  padding: 12px;
+}
+.sectionHeader {
+  margin-bottom: 12px;
+  // font-size: 0.95em;
+  font-weight: bold;
+}
+.priceLine {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  & + .priceLine {
+    margin-top: 10px;
+  }
+}
+/* address */
+.shippingAddressSummary {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  .area, .contact {
+    font-size: 0.9em;
+    color: lignten($color-text, 30%);
+  }
+  .address {
+    font-size: 1.1em;
+    font-weight: bold;
+    margin: 0.2em auto;
+  }
+}
+/* lines */
+.linesSummary {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  .lineImage {
+    width: 60px;
+    height: 60px;
+    border-radius: 6px;
+    overflow: hidden;
+    margin-right: 10px;
+  }
+  padding-bottom: 12px;
+  border-bottom: 1px solid $color-divider;
+}
+.couponCodesSummary {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 16px 0;
+  margin-bottom: -12px;
+}
+/* footer */
+.footer {
+  position: fixed;
+  z-index: $z-index-footer;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  box-shadow: 0 -1px 4px 0 rgba(0, 0, 0, 0.03);
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.buttonPayForOrder {
+  //
+}
+.textTip {
+  font-size: 0.85em;
+  color: $color-text-lighter;
+}
+.caret {
+  color: $color-text-lighter;
+}
+</style>
