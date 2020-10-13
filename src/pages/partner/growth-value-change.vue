7dd1ed0fd575c8ccdc4069500512f48cc8ecaca3
鬼骨孖 <dxd.spirits@gmail.com>
2020-10-13 17:47:36 +0800

    style growth value change page

--- src/pages/partner/growth-value-change.vue
+++ src/pages/partner/growth-value-change.vue
@@ -1,6 +1,19 @@
 <template>
   <view :class="$style['page']">
-    Growth Value Change
+    <view v-for="item in growthValueChanges.data" :key="item.id" :class="$style['item']">
+      <view :class="$style['itemHeader']">
+        <view :class="$style['value']">成长值 +{{ item.growth_value_adjustment }}</view>
+        <view :class="$style['kind']">{{ item.kind === 'order_paid' ? '购买商品' : item.note }}</view>
+        <view :class="$style['timestamp']">{{ item.timestamp|datetime }}</view>
+      </view>
+      <view :class="$style['note']" v-if="item.kind === 'order_paid'">{{ item.note }}</view>
+    </view>
+    <view v-if="growthValueChanges.pending" :class="$style['loading']"><text class="el-icon-loading"></text></view>
+    <view
+      v-else-if="hasMore"
+      :class="$style['loadMore']"
+      @tap="() => fetchGrowthValueChanges({ more: true })"
+    ><text class="el-icon-more"></text></view>
   </view>
 </template>
 
@@ -22,15 +35,21 @@ export default {
     }
   },
   computed: {
-    ...mapState(['customer'])
+    ...mapState(['customer']),
+    hasMore() {
+      return this.growthValueChanges.data.length < this.growthValueChanges.count
+    }
   },
   onReachBottom() {
-    this.fetchGrowthValueChanges({ more: true })
+    if (this.hasMore) {
+      this.fetchGrowthValueChanges({ more: true })
+    }
   },
   mounted() {
     if (!this.customer.isAuthenticated) {
       Taro.redirectTo({ url: '/pages/login/index' })
     } else {
+      // this.updatePageSize(1, { fetch: false })
       this.fetchGrowthValueChanges()
     }
   },
@@ -53,5 +72,46 @@ export default {
 @import "@/styles/variables";
 .page {
   //
+}
+.item {
+  padding: 15px;
+  border-bottom: 1px solid $color-divider;
+}
+.itemHeader {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.value {
+  border-radius: 2px;
+  background-color: #e5c9a4;
+  color: #5c411a;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 2px 8px;
+  width: 90px;
+}
+.kind {
+  flex: 1;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+.timestamp {
+  // margin-left: auto;
+  font-size: 12px;
+  color: $color-text-lighter;
+}
+.note {
+  width: 100%;
+  flex: 1;
+  font-size: 13px;
+  color: $color-text-lighter;
+  margin-top: 5px;
+}
+.loadMore, .loading {
+  text-align: center;
+  font-size: 20px;
+  padding: 10px 15px;
+  color: $color-text-lighter;
 }
 </style>
--- src/pages/partner/rebate.vue
+++ src/pages/partner/rebate.vue
@@ -1,6 +1,21 @@
 <template>
   <view :class="$style['page']">
-    Rebate
+    <view v-for="item in rebates.data" :key="item.id" :class="$style['item']">
+      <view :class="$style['itemHeader']">
+        <view :class="$style['value']">
+          <price :price="item.amount" :highlight="true"></price>
+        </view>
+        <view :class="$style['kind']">{{ item|kindText }}</view>
+        <view :class="$style['timestamp']">{{ item.timestamp|datetime }}</view>
+      </view>
+      <view :class="$style['note']">{{ item.note }}</view>
+    </view>
+    <view v-if="rebates.pending" :class="$style['loading']"><text class="el-icon-loading"></text></view>
+    <view
+      v-else-if="hasMore"
+      :class="$style['loadMore']"
+      @tap="() => fetchRebates({ more: true })"
+    ><text class="el-icon-more"></text></view>
   </view>
 </template>
 
@@ -9,6 +24,7 @@ import _ from 'lodash'
 import Taro from '@tarojs/taro'
 import { API } from '@/utils/api'
 import { mapState } from 'vuex'
+import Price from '@/components/Price'
 import ListTable from '@/mixins/ListTable'
 
 export default {
@@ -16,21 +32,30 @@ export default {
   mixins: [
     ListTable('rebates', { urlRoot: '/substores/partners/rebate/' })
   ],
+  components: {
+    Price
+  },
   data() {
     return {
       //
     }
   },
   computed: {
-    ...mapState(['customer'])
+    ...mapState(['customer']),
+    hasMore() {
+      return this.rebates.data.length < this.rebates.count
+    }
   },
   onReachBottom() {
-    this.fetchRebates({ more: true })
+    if (this.hasMore) {
+      this.fetchRebates({ more: true })
+    }
   },
   mounted() {
     if (!this.customer.isAuthenticated) {
       Taro.redirectTo({ url: '/pages/login/index' })
     } else {
+      // this.updatePageSize(1, { fetch: false })
       this.fetchRebates()
     }
   },
@@ -44,6 +69,15 @@ export default {
       }
       Taro.hideNavigationBarLoading()
     },
+  },
+  filters: {
+    kindText(item) {
+      return {
+        'order_paid': '自购返利',
+        'referee_order_paid': '推荐购买返利',
+        'referral': '邀请会员返利',
+      }[item.kind]
+    }
   }
 }
 </script>
@@ -53,5 +87,41 @@ export default {
 @import "@/styles/variables";
 .page {
   //
+}
+.item {
+  padding: 15px;
+  border-bottom: 1px solid $color-divider;
+}
+.itemHeader {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.value {
+  font-size: 18px;
+  width: 80px;
+}
+.kind {
+  flex: 1;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+.timestamp {
+  // margin-left: auto;
+  font-size: 12px;
+  color: $color-text-lighter;
+}
+.note {
+  width: 100%;
+  flex: 1;
+  font-size: 13px;
+  color: $color-text-lighter;
+  margin-top: 5px;
+}
+.loadMore, .loading {
+  text-align: center;
+  font-size: 20px;
+  padding: 10px 15px;
+  color: $color-text-lighter;
 }
 </style>
