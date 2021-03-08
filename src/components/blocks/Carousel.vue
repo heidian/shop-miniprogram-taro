3e8eaad716044e0c07ff524516cff112a07c6d94
鬼骨孖 <dxd.spirits@gmail.com>
2021-03-08 17:45:22 +0800

    rerender carousel on update

--- src/components/blocks/Carousel.vue
+++ src/components/blocks/Carousel.vue
@@ -3,11 +3,12 @@
     <view class="carousel" :style="{'paddingTop': paddingTop}">
       <!-- indicatorColor='#999' indicatorActiveColor='#333' -->
       <swiper
+        v-if="carousel"
         class='carousel__swiper'
         :indicatorDots="false" :vertical="false"
         :circular="true" :autoplay="true" :interval="5000" :duration="300"
       >
-        <swiper-item v-for="(item, index) in settingsData.carousel" :key="index">
+        <swiper-item v-for="(item, index) in carousel" :key="index">
           <!-- <image class="carousel__image" :src="optimizeImage(item.image, 600)" mode="aspectFit"></image> -->
           <navigator
             :style="{'backgroundImage':backgroundImageUrl(item.image)}"
@@ -21,6 +22,7 @@
 </template>
 
 <script>
+import _ from 'lodash'
 import { optimizeImage, backgroundImageUrl } from '@/utils/image'
 import parseUrl from '@/utils/parseUrl'
 
@@ -39,7 +41,9 @@ export default {
     }
   },
   data() {
-    return {}
+    return {
+      carousel: _.isEmpty(this.settingsData.carousel) ? null : [ ...this.settingsData.carousel ]
+    }
   },
   computed: {
     paddingTop() {
@@ -57,6 +61,17 @@ export default {
     getOpenType(url) {
       const parse = parseUrl(url)
       return parse.openType
+    }
+  },
+  watch: {
+    'settingsData.carousel': {
+      handler(newVal, oldVal) {
+        this.carousel = null  // 无论如何都先清空 carousel 确保每次修改都会重新渲染 swiper
+        if (!_.isEmpty(newVal)) {
+          this.$nextTick(() => this.carousel = [ ...newVal ] )
+        }
+      },
+      deep: true,
     }
   }
 }
