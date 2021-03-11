5a7090e90ae885d06d73767a6b48a05f0e751c30
鬼骨孖 <dxd.spirits@gmail.com>
2021-03-11 17:53:16 +0800

    setBackgroundColor in header for weapp only

--- src/components/blocks/Header.vue
+++ src/components/blocks/Header.vue
@@ -25,16 +25,17 @@ export default {
     const colors = {}
     _.forEach(['frontColor', 'backgroundColor'], (colorName) => {
       if (this.settingsData[colorName]) {
+        // setNavigationBarColor 只接受 hex 格式的颜色
         colors[colorName] = tinycolor(this.settingsData[colorName]).toHexString()
       }
     })
-    // setNavigationBarColor 只接受 hex 格式的颜色
     Taro.setNavigationBarColor(colors)
-    // if (this.settingsData.backgroundColor) {
-    //   Taro.setBackgroundColor({
-    //     backgroundColorTop: this.settingsData.backgroundColor
-    //   })
-    // }
+    // 只有小程序支持 setBackgroundColor
+    if (Taro.getEnv() === Taro.ENV_TYPE.WEAPP && this.settingsData.backgroundColor) {
+      Taro.setBackgroundColor({
+        backgroundColorTop: tinycolor(this.settingsData.backgroundColor).toHexString()
+      })
+    }
   }
 }
 </script>
