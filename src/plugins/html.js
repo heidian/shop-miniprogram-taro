6428bcece4fb8830296549f77c8b630023d38c6b
鬼骨孖 <dxd.spirits@gmail.com>
2021-03-23 16:41:54 +0800

    add comment

--- src/plugins/html.js
+++ src/plugins/html.js
@@ -6,22 +6,18 @@ import { goToUrl, parseUrl } from '@/utils/url'
 /* 这个只在小程序端有效, H5 其实也应该处理, 只能另外想办法了 */
 Taro.options.html.transformElement = (el) => {
   if (el.h5tagName === 'br') {
-    // console.log(el, el.textContent, el.innerHTML)
     el.textContent = '\n'
   }
   if (el.h5tagName === 'a') {
-    // TODO 这里要研究下, 会不会内存溢出
     const handler = ((href) => {
       return () => goToUrl(href)
     })(el.props.href)
+    /*
+    TODO 这里要研究下, 会不会内存泄露
+    TaroElement 有一个属性 __handlers, 里面记录了节点上绑定的事件,
+    源码里面没体现出来，但是 Taro 很可能在其他地方会销毁这些事件
+    */
     el.addEventListener('tap', handler)
-    // 如果把 a 转换成 navigator, 需要加一段样式 navigator.a { display: inline-block; }
-    // el.nodeName = 'navigator'
-    // el.tagName = 'NAVIGATOR'
-    // const parse = parseUrl(el.props.href)
-    // el.props.url = parse.url
-    // el.props.openType = parse.openType
-    // el.props.hoverClass = 'none'
   }
   const fontSize = _.get(el, 'style.fontSize')
   if (fontSize && /^\d+(\.\d+)?(px)?$/.test(fontSize)) {
@@ -29,3 +25,15 @@ Taro.options.html.transformElement = (el) => {
   }
   return el
 }
+
+
+/*
+// 备份一下把 a 转换成 navigator 的方案, 目前为了确保样式没问题, 采用监听 tap 事件的方案
+// 如果把 a 转换成 navigator, 需要加一段样式 navigator.a { display: inline-block; }
+el.nodeName = 'navigator'
+el.tagName = 'NAVIGATOR'
+const parse = parseUrl(el.props.href)
+el.props.url = parse.url
+el.props.openType = parse.openType
+el.props.hoverClass = 'none'
+*/
